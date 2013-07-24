4c962d80ba12bf2060b87f1775c366022f07d3a0
Dan Addington <daniel.anthony.addington@gmail.com>
2013-07-24 18:28:42 +1000

    fixed associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -17,7 +17,7 @@ class Pin < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
 
-  attr_accessible :title, :address, :user_id
+  attr_accessible :title, :address, :user_id, :category_id
 
   validates_presence_of :title, :address
 end
