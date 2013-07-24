f020524147b994767ff4aeb1e3219e40a6665736
Obi <andrew.obrien1@gmail.com>
2013-07-24 17:29:20 +1000

    start pinr.js, add haml

---
+++ app/assets/javascripts/pinr.js
@@ -0,0 +1,81 @@
+var map;
+var canvas;
+var styles = [
+  {
+    featureType: "all",
+    stylers: [
+      { saturation: -80 }
+    ]
+  },{
+    featureType: "road.arterial",
+    elementType: "geometry",
+    stylers: [
+      { hue: "#00ffee" },
+      { saturation: 50 }
+    ]
+  },{
+    featureType: "poi.business",
+    elementType: "labels",
+    stylers: [
+      { visibility: "off" }
+    ]
+  }
+];
+
+var excited = [
+  {
+    featureType: "all",
+    stylers: [
+      { saturation: -100 }
+    ]
+  },{
+    featureType: "road.arterial",
+    elementType: "geometry",
+    stylers: [
+      { hue: "#00ffee" },
+      { saturation: 100 }
+    ]
+  },{
+    featureType: "poi.business",
+    elementType: "labels",
+    stylers: [
+      { visibility: "off" }
+    ]
+  }
+];
+
+var $category = excited;
+
+var display_map = function (lat, long, zoom) {
+  canvas = $('#map_canvas')[0];
+  if (!canvas) {
+    return; // quits this function if no canvas element present
+  }
+
+  var mapOptions = {
+    center: new google.maps.LatLng(lat, long),
+    zoom: zoom,
+    styles: $category,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+
+  };
+  map = new google.maps.Map(canvas, mapOptions);
+};
+
+
+
+
+var add_marker = function (lat, long, title) {
+  var latlng = new google.maps.LatLng(lat, long);
+  var marker = new google.maps.Marker({
+    position: latlng,
+    map: map,
+    title: title
+  });
+};
+
+$(document).ready(function () {
+  display_map(-33.89336, 151.217167, 13);
+
+
+});
